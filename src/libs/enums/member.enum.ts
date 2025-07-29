85783b93b580cd7fe36725f8efe1494dfc89a064
mahmud1995 <makhmudgo95@gmail.com>
2025-07-29 18:31:23 +0900

    feat:Member Schema is created

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,11 @@
+export enum MemberType {
+  SUPERADMIN = "SUPERADMIN",
+  RESTAURANT = "RESTAURANT",
+  USER = "USER",
+}
+
+export enum MemberStatus {
+  ACTIVE = "ACTIVE",
+  BLOCK = "BLOCK",
+  DELETE = "DELETED",
+}
