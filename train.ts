56de5ab2fced7d7a97a1b0a34429090521d047b6
mahmud1995 <makhmudgo95@gmail.com>
2025-08-01 23:24:12 +0900

    feat: Function -2 is modified For Promise.all

--- train.ts
+++ train.ts
@@ -1,18 +1,34 @@
 console.log("train test");
-async function readAllFuncs(): Promise<string> {
-    console.log("🚀 Starting all tasks...");
+async function readAllFuncs(): Promise<string[]> {
+  console.log("🚀 Starting all tasks...");
 
-    try {
-        // const results = Promise.all()
-    }
+  try {
+    const results = await Promise.all([walkDog(), cleanKitchen()]);
+    console.log("✅ All mission is completed!");
+    console.log("📋 Results:", results);
+    return results;
+  } catch (error) {
+    console.log("❌ Error:", error);
+    throw error;
+  }
 }
 
 // Function - 1
 function walkDog(): Promise<string> {
-    return new Promise((resolve)=> {
-        setTimeout(() => {
-            console.log("🐕 Dog walk completed!");
-            resolve("You walked the dog")
-        }, 2000)
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("🐕 Dog walk completed!");
+      resolve("You walked the dog");
+    }, 2000);
+  });
+}
+// Function - 2
+function cleanKitchen(): Promise<string> {
+  console.log("🧽 Starting to clean kitchen...");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("🧽 Kitchen cleaning completed!");
+      resolve("You cleaned the Kitchen");
+    }, 3000);
+  });
 }
